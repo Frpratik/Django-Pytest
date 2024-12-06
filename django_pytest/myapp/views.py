554f6cb0a0b18a9e387b7d827e9d1732e46f7d69
Pratik Ghuge <pratik.g@mobitrail.local>
2024-12-06 16:20:56 +0530

    Added Model, Views, Urls, Form

--- django_pytest/myapp/views.py
+++ django_pytest/myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def blog_list(request):
+    blogs = Blog.objects.all()
+    return render(request, "blog_list.html", {"blogs": blogs})
